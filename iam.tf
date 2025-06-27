4874627f679e39826180990bd7e0891c937af28e
bhavanigowda987 <bhavanijd51@gmail.com>
2025-06-27 16:56:57 +0530

    Create iam.tf

---
+++ iam.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "redshift_role" {
+  name = "redshift-access-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "redshift.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "redshift_s3" {
+  role       = aws_iam_role.redshift_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
