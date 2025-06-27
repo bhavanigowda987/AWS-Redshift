06630ac6dc7e17a52870cb18152851b944a1259c
bhavanigowda987 <bhavanijd51@gmail.com>
2025-06-27 16:57:38 +0530

    Update monitoring.tf

--- monitoring.tf
+++ monitoring.tf
@@ -1,1 +1,15 @@
+resource "aws_cloudwatch_log_group" "redshift" {
+  name = "/aws/redshift/${var.redshift_cluster_identifier}"
+}
 
+resource "aws_cloudtrail" "redshift_trail" {
+  name                          = "redshift-trail"
+  s3_bucket_name                = aws_s3_bucket.redshift_logs.bucket
+  include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_log_file_validation    = true
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = true
+  }
+}
